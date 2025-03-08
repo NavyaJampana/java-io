e8705e34c7b592feaa8086a71686fc4c088a727c
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:53:46 +0530

    Add files via upload

---
+++ WriteUsingBufferedOutputStream.java
@@ -0,0 +1,19 @@
+package javaio;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+class WriteUsingBufferedOutputStream {
+    public static void main(String[] args) {
+        String text = "Buffered OutputStream Example.";
+
+        try (BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("output.txt"))) {
+            bos.write(text.getBytes());
+            bos.flush();
+            System.out.println("Text written successfully.");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
