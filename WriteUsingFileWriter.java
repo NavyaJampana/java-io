ab55b69b7c07f6e33993283e75daa16a159a78e7
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 17:54:15 +0530

    Add files via upload

---
+++ WriteUsingFileWriter.java
@@ -0,0 +1,15 @@
+package javaio;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class WriteUsingFileWriter {
+    public static void main(String[] args) {
+        try (FileWriter fw = new FileWriter("output.txt")) {
+            fw.write("FileWriter Example.");
+            System.out.println("Text written successfully.");
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
+
